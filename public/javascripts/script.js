084ab7d2651e06757dc58d52df1d1078be95cbb5
Byjusmohit <mohit.barua@byjus.com>
2023-01-14 01:07:14 +0530

    New Code

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,38 +1,48 @@
-const socket = io();
-// socket.emit("new user", "mohit");
+const socket_backend = io();
 
-click_to_record.addEventListener('click', async function(){
-    var speech = true;
-    window.SpeechRecognition = window.webkitSpeechRecognition;
+navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    console.log({ stream })
+    if (!MediaRecorder.isTypeSupported('audio/webm'))
+    
+    return alert('Browser not supported')
+    const mediaRecorder = new MediaRecorder(stream, {
+        mimeType: 'audio/webm',
+    })
 
-    const recognition = new SpeechRecognition();
-    recognition.interimResults = true;
+    const socket = new WebSocket('wss://api.deepgram.com/v1/listen', [
+      'token',
+      '47568cc2314f5bdbb3f9768a139ae84df01201f5',
+    ])
 
-    // recognition.addEventListener('result', e => {
-    //     const transcript = Array.from(e.results)
-    //         .map(result => result[0])
-    //         .map(result => result.transcript)
-    //         .join('')
-    //     socket.emit("voice", transcript);
-    // });
-
-    recognition.addEventListener('result', e => {
-        const transcript = Array.from(e.results)
-            .map((result) => {
-                // console.log(result)
-                // if(result['isFinal']){
-                    return result[0].transcript
-                // }
-            })
-        // if( !transcript.includes(undefined) && !transcript.includes(null))
-        socket.emit("voice", transcript);
-        console.log(transcript)
-    });
-   
-    if (speech == true) {
-        recognition.start();
+    socket.onopen = () => {
+      document.querySelector('#status').textContent = 'Connected'
+      console.log({ event: 'onopen' })
+      mediaRecorder.addEventListener('dataavailable', async (event) => {
+        if (event.data.size > 0 && socket.readyState == 1) {
+          socket.send(event.data)
+        }
+      })
+      mediaRecorder.start(1000)
     }
-})
+
+    socket.onmessage = (message) => {
+      const received = JSON.parse(message.data)
+      const transcript = received.channel.alternatives[0].transcript
+      if (transcript && received.is_final) {
+        console.log(transcript)
+        socket_backend.emit("voice", transcript);
+      }
+    }
+
+    socket.onclose = () => {
+      console.log({ event: 'onclose' })
+    }
+
+    socket.onerror = (error) => {
+      console.log({ event: 'onerror', error })
+    }
+  })
+
 
 function say(m) {
     console.log("==>",m)
@@ -44,12 +54,11 @@ function say(m) {
     msg.rate = 1;
     msg.pitch = 0.8;
     msg.text = m;
-    msg.lang = 'en-US';
+    msg.lang = 'en';
     speechSynthesis.speak(msg);
 }
 
-
-socket.on("voice",(data)=>{
+socket_backend.on("voice",(data)=>{
     document.getElementById("convert_text").innerHTML = data;
     say(data)
 })
